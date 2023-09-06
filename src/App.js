1ca3a6ca308b20a8fd48d9669d263ebe6ff02b4c
Maaximuz <diogo.cesar93@gmail.com>
2023-09-05 23:01:23 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import './App.css';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const Form = styled.div`
+  background-color: #f5f5f5;
+  border-radius: 8px;
+  padding: 20px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  width: 300px;
+`;
+
+const Title = styled.h1`
+  margin-bottom: 20px;
+`;
+
+const InputLabel = styled.label`
+  font-size: 14px;
+  margin-bottom: 5px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  margin-bottom: 15px;
+`;
+
+const Button = styled.button`
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  padding: 10px 15px;
+  cursor: pointer;
+`;
+
+
+function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleLogin = () => {
+    if (email === 'eduardo.lino@pucpr.br' && password === '123456') {
+      setMessage('Acessado com sucesso!'); 
+    } else {
+      setMessage('Usuário ou senha incorretos!');
+    }
+  };
+
+  return (
+    <Container>
+      <Form>
+        <Title>Login</Title>
+        <div>
+          <InputLabel>Email:</InputLabel>
+          <Input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <InputLabel>Senha:</InputLabel>
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <Button onClick={handleLogin}>Acessar</Button>
+        <p>{message}</p>
+      </Form>
+    </Container>
+  );
+}
+
+export default App;
